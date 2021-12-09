2242a35c3eebdc45a983e7e0ba155627c8e96ead
Magnus Halvorsen <magnus.halvorsen@kdi.kongsberg.com>
2021-12-09 20:32:27 +0100

    Initialize window with color, and exit code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+typedef uint32_t u32;
+typedef int32_t i32;
+typedef uint8_t u8;
+typedef int8_t i8;
+typedef float f32;
+typedef double f64;
+
 SDL_Window *window;
 SDL_Renderer *renderer;
 bool is_running;
+u32 *color_buffer;
+
+const u32 WINDOW_HEIGHT = 600;
+const u32 WINDOW_WIDTH = 800;
 
 bool initialize_window() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -11,7 +22,7 @@ bool initialize_window() {
         return false;
     }
 
-    window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
+    window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_BORDERLESS);
 
     if (!window) {
         fprintf(stderr, "Error creating SDL window.\n");
@@ -28,12 +39,59 @@ bool initialize_window() {
     return true;
 }
 
-int main(void) {
-    is_running = initialize_window();
-    if (!is_running) {
-        fprintf(stderr, "Error initializing window.\n");    
-        return -1;
+void setup(void) {
+    color_buffer = (u32*) callco(sizeof(u32)*WINDOW_WIDTH*WINDOW_HEIGHT);
+} 
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type)
+    {
+    case SDL_QUIT:
+        is_running = false;
+        break;    
+    case SDL_KEYDOWN: 
+        if (event.key.keysym.sym == SDLK_ESCAPE) {
+            is_running = false;
+        }
+        break;
+    default:
+        break;
     }
-    fprintf(stdout, "Window initialized.\n");    
+}
+
+void update(void) {
+
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+}
+
+void destory_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(void) {
+    is_running = initialize_window();    
+
+    setup();
+    
+    while(is_running) {
+        process_input();
+        update();
+        render();
+    }
+
+    destory_window();
+
     return 0;
 }
